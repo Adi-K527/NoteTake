51261804a1d141493891e03a424291bf496ce4b6
Adi-K527 <adikandakurtikar2@gmail.com>
2023-02-17 23:36:46 -0600

    test

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors  from "cors";
+
+
+
+const app = express()
+
+
+app.use(cors())
+//app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }))
+
+
+
+app.listen(5000, () => {console.log("Server is listening on port 5000")})
+
+
